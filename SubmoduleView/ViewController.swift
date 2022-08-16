d4694af15faa5618ce1f8a7d6fc34fecd45ff462
Bruce <VDBruceWu@taiwanmobile.com>
2022-08-16 22:13:43 +0800

    add GitTest

--- SubmoduleView/ViewController.swift
+++ SubmoduleView/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var customView: CustomView = {
+        let v = CustomView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(customView)
+        customView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        customView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        customView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        customView.heightAnchor.constraint(equalToConstant: 100).isActive = true
     }
 
 
